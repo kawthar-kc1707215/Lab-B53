e8c881d7ad8e3fd0b50143c0ed90c079b03ffeca
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-15 11:53:10 +0300

    lab 5

---
+++ Lab5-RecyclerView/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/MainActivity.kt
@@ -0,0 +1,12 @@
+package cmps312.lab.qatar2022
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qatar2022.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
