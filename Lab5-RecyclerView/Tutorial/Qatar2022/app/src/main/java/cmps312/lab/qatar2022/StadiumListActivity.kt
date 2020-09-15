845b6c5cbeb02373eb974d0515a2408e6259ca57
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-15 14:24:56 +0300

    updated

--- Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/StadiumListActivity.kt
+++ Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/StadiumListActivity.kt
@@ -2,11 +2,19 @@ package cmps312.lab.qatar2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import cmps312.lab.qatar2022.adapter.StadiumAdapter
+import cmps312.lab.qatar2022.model.StadiumRepository
 import com.example.qatar2022.R
+import kotlinx.android.synthetic.main.activity_stadium_list.*
 
 class StadiumListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stadium_list)
+
+        StadiumRepository.initStadiums(this) //this will read and populate data
+        stadiumsRv.adapter = StadiumAdapter(StadiumRepository.stadiums, this)
+        stadiumsRv.layoutManager = LinearLayoutManager(this)
     }
 }
--- Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/adapter/StadiumAdapter.kt
+++ Lab5-RecyclerView/Tutorial/Qatar2022/app/src/main/java/cmps312/lab/qatar2022/adapter/StadiumAdapter.kt
@@ -1,4 +1,44 @@
 package cmps312.lab.qatar2022.adapter
 
-class StadiumAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cmps312.lab.qatar2022.model.Stadium
+import com.example.qatar2022.R
+
+class StadiumAdapter(val stadiums: List<Stadium>, private val context: Context)
+    : RecyclerView.Adapter<StadiumAdapter.StadiumViewHolder>() {
+
+    //step 1 create the view holder
+    class StadiumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val nameTv = itemView.findViewById<TextView>(R.id.nameTv)
+        val cityTv = itemView.findViewById<TextView>(R.id.cityTv)
+        val statusTv = itemView.findViewById<TextView>(R.id.statusTv)
+        val stadiumIv = itemView.findViewById<ImageView>(R.id.stadiumIv)
+    }
+
+    //step 3
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StadiumViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.list_item_stadium, parent , false)
+        return StadiumViewHolder(view)
+    }
+    //step 4 injecting the data
+    override fun onBindViewHolder(holder: StadiumViewHolder, position: Int) {
+        holder.nameTv.text = stadiums[position].name
+        holder.cityTv.text = stadiums[position].name
+        holder.statusTv.text = stadiums[position].name
+
+        val imageName = stadiums[position].imageName
+        val imageId = context.resources.getIdentifier(imageName, "drawable" , context.packageName)
+        holder.stadiumIv.setImageResource(imageId)
+    }
+    //step 2
+    override fun getItemCount() = stadiums.size
+
+
 }
