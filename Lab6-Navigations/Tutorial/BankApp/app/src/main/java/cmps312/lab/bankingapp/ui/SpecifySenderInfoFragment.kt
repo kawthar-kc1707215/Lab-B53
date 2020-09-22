1b5b78c96c1e8cc9b28e32a7c80a5c12e83bc3c8
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-22 13:59:26 +0300

    update 1:59

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifySenderInfoFragment.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifySenderInfoFragment.kt
@@ -7,7 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import cmps312.lab.bankingapp.R
+import cmps312.lab.bankingapp.model.Transaction
+
+
 import kotlinx.android.synthetic.main.fragment_specify_sender_info.view.*
+import kotlinx.android.synthetic.main.fragment_specify_sender_info.view.cancelBtn
+import kotlinx.android.synthetic.main.fragment_specify_sender_info.view.nextBtn
+import kotlinx.android.synthetic.main.fragment_specify_sender_info.view.senderNameEdt
 
 //itemView
 
@@ -20,13 +26,25 @@ class SpecifySenderInfoFragment : Fragment() {
 
         val rootView =  inflater.inflate(R.layout.fragment_specify_sender_info, container, false)
 
+
         rootView.apply {
+
             nextBtn.setOnClickListener {
+                val transaction = Transaction()
+                transaction.senderName  = senderNameEdt.text.toString()
+                transaction.senderAccountNo = senderAccountNoEdt.text.toString()
+                transaction.amount = amountEdt.text.toString().toDouble()
+
+                val action = SpecifySenderInfoFragmentDirections
+                    .actionSpecifySenderInfoFragmentToSpecifyReceiverInfoFragment(transaction)
+
                 Navigation.findNavController(rootView)
-                    .navigate(R.id.action_specifySenderInfoFragment_to_specifyReceiverInfoFragment)
+                    .navigate(action)
             }
 
-            cancelBtn.setOnClickListener {}
+            cancelBtn.setOnClickListener {
+
+            }
         }
 
         return rootView
