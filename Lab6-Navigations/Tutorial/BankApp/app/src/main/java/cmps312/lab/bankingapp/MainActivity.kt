5e02e39631481df88a6138196a36d0396eb4ffaa
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-22 12:30:38 +0300

    update

---
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package cmps312.lab.bankingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cmps312.lab.bankingapp.model.Transaction
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
