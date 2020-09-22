964220a5ade30ff25e0c7c540eb2fbeae3399861
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-22 13:22:17 +0300

    update 1:22

--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifyReceiverInfoFragment.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifyReceiverInfoFragment.kt
@@ -35,7 +35,7 @@ class SpecifyReceiverInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specify_receiver_info, container, false)
+        return inflater.inflate(R.layout.fragment_specify_receiver_information, container, false)
     }
 
     companion object {
--- Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifySenderInfoFragment.kt
+++ Lab6-Navigations/Tutorial/BankApp/app/src/main/java/cmps312/lab/bankingapp/ui/SpecifySenderInfoFragment.kt
@@ -5,56 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import cmps312.lab.bankingapp.R
+import kotlinx.android.synthetic.main.fragment_specify_sender_info.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//itemView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SpecifySenderInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SpecifySenderInfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specify_sender_info, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpecifySenderInfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpecifySenderInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val rootView =  inflater.inflate(R.layout.fragment_specify_sender_info, container, false)
+
+        rootView.apply {
+            nextBtn.setOnClickListener {
+                Navigation.findNavController(rootView)
+                    .navigate(R.id.action_specifySenderInfoFragment_to_specifyReceiverInfoFragment)
             }
+
+            cancelBtn.setOnClickListener {}
+        }
+
+        return rootView
     }
 }
