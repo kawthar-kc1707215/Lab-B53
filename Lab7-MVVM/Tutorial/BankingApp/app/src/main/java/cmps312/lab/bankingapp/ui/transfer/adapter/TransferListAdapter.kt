07ce79156fa35a573e19a10688583ae956cc53f6
Abdulahi Hassen <abdala.baaji@gmail.com>
2020-09-29 13:47:53 +0300

    update 1:47

--- Lab7-MVVM/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/transfer/adapter/TransferListAdapter.kt
+++ Lab7-MVVM/Tutorial/BankingApp/app/src/main/java/cmps312/lab/bankingapp/ui/transfer/adapter/TransferListAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import cmps312.lab.bankingapp.R
+import cmps312.lab.bankingapp.databinding.ListItemTransferBinding
 import cmps312.lab.bankingapp.model.Transfer
 import kotlinx.android.synthetic.main.list_item_transfer.view.*
 
@@ -16,20 +18,20 @@ class TransferListAdapter(private val context: Context, private val listener: (T
     init {
         transfers = listOf<Transfer>()
     }
-    inner class TransactionViewHolder(val itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
+    inner class TransactionViewHolder(val binding: ListItemTransferBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(transfer: Transfer) {
-           itemView.apply {
-               beneficiaryNameTv.text = transfer.beneficiaryName
-               accountNoTv.text = transfer.beneficiaryAccountNo
-               amountTv.text = transfer.amount.toString()
-           }
+            binding.transfer = transfer
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.list_item_transfer, parent, false)
-        return TransactionViewHolder(view)
+
+        val binding:ListItemTransferBinding
+                = DataBindingUtil.inflate(LayoutInflater.from(context),R.layout.list_item_transfer, parent, false )
+
+        return TransactionViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) = holder.bind(transfers[position])
